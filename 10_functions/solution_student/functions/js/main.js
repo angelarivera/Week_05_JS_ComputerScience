8f0cb92e7882a64318f33ca2356b8fa7f0dcb319
Angela Rivera <riveracangela@gmail.com>
2016-01-14 20:27:53 -0600

    changes cause Matt shared too much

--- 10_functions/solution_student/functions/js/main.js
+++ 10_functions/solution_student/functions/js/main.js
@@ -5,10 +5,12 @@
 
 // 1. Declare a function. Give it the name "addSunshine".
 	// a. Inside the function, add the sunny class to the body
-
+	function addSunshine(){
+		$('body').addClass('sunny'); 
+	}
 
 // 2. Call the function
-
+	addSunshine(); 
 
 
 // -------------------------------------------------------------------------------
@@ -17,6 +19,11 @@
 
 // 1. Declare a function. Name it "greet". Pass in "firstName" as the parameter. 
 	// a. Inside the function, change the text of the h1 to 'Hello ' + firstName
-
+	function greet(firstName){
+		$('h1').html('Hello ' + firstName);
+		console.log('Hello' + firstName);
+	}
 
 // 2. Call the function, passing in your name (as a string) as the argument.
+
+	greet('Angie');
